f5be2956c0226ab4747a4a85d21df0cd1efa264d
oni <nuratjahanoni2005@gmail.com>
2025-10-06 18:13:39 +0600

    th

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-//exam 2
+//Difine variable in javascript with example
+let name = "Nazmul";
+const age = 10;
+var n = 30;
+
+//what is object : Object is stepment , this used by {} .  example
+const bike  = {
+  y = "Baik" ,
+  color = blue ,
+  price = 10000 ,
+}
+//what is arry : arry is stepment , this used by [], example
+const man = [x = "man", a = 10]
+
+//section C
+// 26
+ 
+let x = 5 ;
+let y = '5';
+
+console.log(x == y , x === y);
+
+
+// 27
+let arr = [1,2,3];
+arr.pop();
+arr.push(4);
+console.log(arr);
+
+//28
+let obj = {name:'John', age : 25};
+console.log(obj.name);
+
+//29
+let numbers = [2,4,6,8];
+console.log(numbers[2]);
+
+//30
+let text = "JavaSceript";
+console.log(text.slice(4,10))
+
+//scetion D
+// 31
+function num () {
+    let num = 6 ;
+   if (num % 2 == 0){
+    console.log("Even")
+   }
+   else {
+    console.log("Folse")
+   }
+}
+
